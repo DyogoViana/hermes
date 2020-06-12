ec9b15086400da19baf532d99ef2f7b893ea422a
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-06-12 18:07:09 -0300

    Criação do HEADER, com logo e ícone do carrinho de compras e background.

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 export default function Routes() {
 	return (
 		<Switch>
-			<Route path="/" exact component={Cart} />
 			<Route path="/" exact component={Home} />
+			<Route path="/" exact component={Cart} />
 		</Switch>
 	);
 }
