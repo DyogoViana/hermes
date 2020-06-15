a02c734c1a055036f6441d460c92dec9d212be7e
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-06-15 18:15:15 -0300

    Estilização da Home.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,117 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-	return <div />;
+	return (
+		<ProductList>
+			<li>
+				<img
+					src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTcgQtXXfJTWsKpIUjVUq_8NbEFDTSaIsNi6tL7ruN5TaWY5hiup9K1sWE34Zwucl9Hrjox3fZ8etCw7UVt30xWH-izSqxwcQ&usqp=CAY"
+					alt="Air Jordan Retro"
+				/>
+
+				<strong>Air Jordan Retro</strong>
+				<span>R$ 1249,99</span>
+
+				<button type="button">
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+
+					<span>Adicionar ao carrinho</span>
+				</button>
+			</li>
+
+			<li>
+				<img
+					src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTcgQtXXfJTWsKpIUjVUq_8NbEFDTSaIsNi6tL7ruN5TaWY5hiup9K1sWE34Zwucl9Hrjox3fZ8etCw7UVt30xWH-izSqxwcQ&usqp=CAY"
+					alt="Air Jordan Retro"
+				/>
+
+				<strong>Air Jordan Retro</strong>
+				<span>R$ 1249,99</span>
+
+				<button type="button">
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+
+					<span>Adicionar ao carrinho</span>
+				</button>
+			</li>
+
+			<li>
+				<img
+					src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTcgQtXXfJTWsKpIUjVUq_8NbEFDTSaIsNi6tL7ruN5TaWY5hiup9K1sWE34Zwucl9Hrjox3fZ8etCw7UVt30xWH-izSqxwcQ&usqp=CAY"
+					alt="Air Jordan Retro"
+				/>
+
+				<strong>Air Jordan Retro</strong>
+				<span>R$ 1249,99</span>
+
+				<button type="button">
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+
+					<span>Adicionar ao carrinho</span>
+				</button>
+			</li>
+
+			<li>
+				<img
+					src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTcgQtXXfJTWsKpIUjVUq_8NbEFDTSaIsNi6tL7ruN5TaWY5hiup9K1sWE34Zwucl9Hrjox3fZ8etCw7UVt30xWH-izSqxwcQ&usqp=CAY"
+					alt="Air Jordan Retro"
+				/>
+
+				<strong>Air Jordan Retro</strong>
+				<span>R$ 1249,99</span>
+
+				<button type="button">
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+
+					<span>Adicionar ao carrinho</span>
+				</button>
+			</li>
+
+			<li>
+				<img
+					src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTcgQtXXfJTWsKpIUjVUq_8NbEFDTSaIsNi6tL7ruN5TaWY5hiup9K1sWE34Zwucl9Hrjox3fZ8etCw7UVt30xWH-izSqxwcQ&usqp=CAY"
+					alt="Air Jordan Retro"
+				/>
+
+				<strong>Air Jordan Retro</strong>
+				<span>R$ 1249,99</span>
+
+				<button type="button">
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+
+					<span>Adicionar ao carrinho</span>
+				</button>
+			</li>
+
+			<li>
+				<img
+					src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTcgQtXXfJTWsKpIUjVUq_8NbEFDTSaIsNi6tL7ruN5TaWY5hiup9K1sWE34Zwucl9Hrjox3fZ8etCw7UVt30xWH-izSqxwcQ&usqp=CAY"
+					alt="Air Jordan Retro"
+				/>
+
+				<strong>Air Jordan Retro</strong>
+				<span>R$ 1249,99</span>
+
+				<button type="button">
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+
+					<span>Adicionar ao carrinho</span>
+				</button>
+			</li>
+		</ProductList>
+	);
 }
